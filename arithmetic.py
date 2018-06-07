121ca8f23dae8a41792d8289fb09ad6917eb15bb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-06 18:00:43 -0700

    Further study

--- arithmetic.py
+++ arithmetic.py
@@ -1,31 +1,33 @@
 """Math functions for calculator."""
 
+lst = []
 
-def add(num1, num2):
+
+def add(lst):
     """Return the sum of the two inputs."""
+    nums = len(range(lst[1], lst[-1]))
+    for num in nums:
+        nu
 
-    return num1 + num2
-
-
-def subtract(num1, num2):
+def subtract(lst):
     """Return the second number subtracted from the first."""
 
     return num1 - num2
 
 
-def multiply(num1, num2):
+def multiply(lst):
     """Multiply the two inputs together."""
 
     return num1 * num2
 
 
-def divide(num1, num2):
+def divide(lst):
     """Divide the first input by the second, returning a floating point."""
 
     return num1 / num2
 
 
-def square(num1):
+def square(lst):
     """Return the square of the input."""
 
     # Needs only one argument
@@ -33,7 +35,7 @@ def square(num1):
     return num1 * num1
 
 
-def cube(num1):
+def cube(lst):
     """Return the cube of the input."""
 
     # Needs only one argument
@@ -41,13 +43,13 @@ def cube(num1):
     return num1 * num1 * num1
 
 
-def power(num1, num2):
+def power(lst):
     """Raise num1 to the power of num and return the value."""
 
     return num1 ** num2  # ** = exponent operator
 
 
-def mod(num1, num2):
+def mod(lst):
     """Return the remainder of num / num2."""
 
     return num1 % num2
